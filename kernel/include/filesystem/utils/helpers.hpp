792aa266baf98f41b1e299dc661de6947866768a
Luke Matthes <LukeMatthes@web.de>
2026-06-19 01:29:38 +0200

    Update the file system inode header comment.

--- kernel/include/filesystem/utils/helpers.hpp
+++ kernel/include/filesystem/utils/helpers.hpp
@@ -5,10 +5,18 @@ LICENSE:
     https://github.com/Lukenar11/MoleculeOS/blob/main/LICENSE
 
 DESCRIPTION:
-    This file contains all meta-information about the file system.
+    This file contains all meta-informations and 
+    the inode structireabout the file system.
 
 NOTES:
-    Instances are created from the inodes, representing the respective files.
+    Eatch inode represents a file with meta infos,
+    the inode contains the file name, frmat, the data, the size and a using info.
+
+    An array of inode instances is created from the inode structure, 
+    which then represents all the files.
+
+    "Inode" is an abbreviation that stands for "Inodex Node", 
+    but I use the abbreviation because it is the common term and the full term is rarely used.
 */
 
 #pragma once
