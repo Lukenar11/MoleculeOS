8c1c17c3fed12df0f22105226e949d4fc178e45d
Luke Matthes <LukeMatthes@web.de>
2026-06-28 18:43:03 +0200

    Update trigger_interrupt.asm,

--- arch/i386/kernel/src/system/trigger_interrupt.asm
+++ arch/i386/kernel/src/system/trigger_interrupt.asm
@@ -1,27 +1,31 @@
 global trigger_interrupt
 
+%define INTERRUPT_TABLE_SIZE 256
+%define FIRST_INTERRUPT_TABLE_ENTRY 0
+%define LAST_INTERRUPT_TABLE_ENTRY 255
+
 section .text
 
-trigger_interrupt:
-    mov eax, [esp+4]
-    cmp eax, 255
-    ja  .done
-    jmp [interrupt_table + eax*4]
-
-.done:
-    ret
-
 interrupt_table:
-%assign i 0
-%rep 256
+%assign i FIRST_INTERRUPT_TABLE_ENTRY
+%rep INTERRUPT_TABLE_SIZE
     dd int_%+i
 %assign i i+1
 %endrep
 
-%assign i 0
-%rep 256
+%assign i FIRST_INTERRUPT_TABLE_ENTRY
+%rep INTERRUPT_TABLE_SIZE
 int_%+i:
     int i
     ret
 %assign i i+1
 %endrep
+
+trigger_interrupt:
+    mov eax, [esp+4]
+    cmp eax, LAST_INTERRUPT_TABLE_ENTRY
+    ja .done
+    jmp [interrupt_table+eax*4]
+
+.done:
+    ret
