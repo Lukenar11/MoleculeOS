8b19b5cf0895d21769546457064a8ad81dc34cfa
Luke Matthes <LukeMatthes@web.de>
2026-07-03 10:17:06 +0200

    Fix unlikely attribute syntax in echo.cpp

--- shell/src/commands/echo.cpp
+++ shell/src/commands/echo.cpp
@@ -106,9 +106,9 @@ namespace
         }
 
         uint32_t j = 0;
-        for (; i < arguments.size(); i++) [[unlikely]] {
+        for (; i < arguments.size(); i++) {
             const char argument = arguments[i];
-            if ((argument == spache_char) || (argument == null_char)) [[unlikely]]
+            if ((argument == spache_char) || (argument == null_char)) [[unlikely]
                 break;
 
             filename[j++] = argument;
