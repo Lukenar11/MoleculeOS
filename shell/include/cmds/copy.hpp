d9f23966fca42976290a666d9eb553bf2511b6a1
Luke Matthes <LukeMatthes@web.de>
2026-07-28 20:29:53 +0200

    Implement a header command to copy.

--- shell/include/cmds/copy.hpp
+++ shell/include/cmds/copy.hpp
@@ -1,3 +1,23 @@
+/**
+LICENSE:
+    Copyright (c) 2026 Lukenar11 (Luke Matthes)
+    MIT licensed
+    https://github.com/Lukenar11/MoleculeOS/blob/main/LICENSE
+
+DESCRIPTION:
+    This file contains the internal implementation of 
+    one of the terminal commands available in MoleculeOS.
+
+    This command copy the content of a file to another file.
+    
+    The commands can be called by the 
+    shell interpreter from a table generated at compile time, 
+    where each entry contains a specific hash and a function call to the 
+    command's internal implementation.
+
+NOTES:
+*/
+
 #pragma once
 
 #include "../utils/cmds_helpers.hpp"
