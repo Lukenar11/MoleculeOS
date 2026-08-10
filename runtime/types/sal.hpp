7e752e534c855df2ab5813beb7cc83c554f35bea
Luke Matthes <LukeMatthes@web.de>
2026-08-10 23:35:15 +0200

    Update "status.hpp" with a "status::" namespace and implement a simple SAL.

---
+++ runtime/types/sal.hpp
@@ -0,0 +1,24 @@
+/**
+LICENSE:
+    Copyright (c) 2026 Lukenar11 (Luke Matthes)
+    MIT Licensed
+    https://github.com/Lukenar11/MoleculeOS/blob/main/LICENSE
+
+DESCRIPTION:
+    This file defines a tiny 'Source Annotation Language' ('SAL'), 
+    to mark in function/method paramater which is used as in or output
+    because methods and functions returns 'void' or 'status_t'.
+
+    SAL Explanation:
+        - IN    => Input
+        - OUT   => Output
+        - INOUT => Input & Output
+
+NOTES:
+*/
+
+#pragma once
+
+#define IN
+#define OUT
+#define INOUT
--- runtime/types/status.hpp
+++ runtime/types/status.hpp
@@ -28,28 +28,31 @@ NOTES:
 
 using status_t = uint32_t;
 
-constexpr status_t STATUS_SUCCESS           = 0x00'0000'00;
-constexpr status_t STATUS_FAIL              = 0x00'0000'01;
-constexpr status_t STATUS_INVALID_PARAMETER = 0x00'0000'02;
-constexpr status_t STATUS_NOT_FOUND         = 0x00'0000'03;
-constexpr status_t STATUS_OUT_OF_MEMORY     = 0x00'0000'04;
-constexpr status_t STATUS_IO_ERROR          = 0x00'0000'05;
-constexpr status_t STATUS_END_OF_FILE       = 0x00'0000'06;
-
-constexpr status_t STATUS_NULL_POINTER         = 0x01'0000'00;
-constexpr status_t STATUS_INVALID_POINTER      = 0x01'0000'01;
-constexpr status_t STATUS_UNALIGNED_POINTER    = 0x01'0000'02;
-constexpr status_t STATUS_POINTER_OUT_OF_RANGE = 0x01'0000'03;
-
-constexpr status_t STATUS_HEAP_CORRUPTED  = 0x02'0000'00;
-constexpr status_t STATUS_HEAP_EXHAUSTED  = 0x02'0000'01;
-constexpr status_t STATUS_ALIGNMENT_ERROR = 0x02'0000'02;
-
-constexpr status_t STATUS_FS_CORRUPTED      = 0x03'0000'00;
-constexpr status_t STATUS_FS_INVALID_INODE  = 0x03'0000'01;
-constexpr status_t STATUS_FS_INVALID_BLOCK  = 0x03'0000'02;
-constexpr status_t STATUS_FS_OUT_OF_SPACE   = 0x03'0000'03;
-
-constexpr status_t STATUS_ATA_TIMEOUT      = 0x04'0000'00;
-constexpr status_t STATUS_ATA_DEVICE_FAULT = 0x04'0000'01;
-constexpr status_t STATUS_ATA_BAD_SECTOR   = 0x04'0000'02;
+namespace status
+{
+    constexpr status_t SUCCESS           = 0x00'0000'00;
+    constexpr status_t FAIL              = 0x00'0000'01;
+    constexpr status_t INVALID_PARAMETER = 0x00'0000'02;
+    constexpr status_t NOT_FOUND         = 0x00'0000'03;
+    constexpr status_t OUT_OF_MEMORY     = 0x00'0000'04;
+    constexpr status_t IO_ERROR          = 0x00'0000'05;
+    constexpr status_t END_OF_FILE       = 0x00'0000'06;
+    
+    constexpr status_t NULL_POINTER         = 0x01'0000'00;
+    constexpr status_t INVALID_POINTER      = 0x01'0000'01;
+    constexpr status_t UNALIGNED_POINTER    = 0x01'0000'02;
+    constexpr status_t POINTER_OUT_OF_RANGE = 0x01'0000'03;
+    
+    constexpr status_t HEAP_CORRUPTED  = 0x02'0000'00;
+    constexpr status_t HEAP_EXHAUSTED  = 0x02'0000'01;
+    constexpr status_t ALIGNMENT_ERROR = 0x02'0000'02;
+    
+    constexpr status_t FS_CORRUPTED      = 0x03'0000'00;
+    constexpr status_t FS_INVALID_INODE  = 0x03'0000'01;
+    constexpr status_t FS_INVALID_BLOCK  = 0x03'0000'02;
+    constexpr status_t FS_OUT_OF_SPACE   = 0x03'0000'03;
+    
+    constexpr status_t ATA_TIMEOUT      = 0x04'0000'00;
+    constexpr status_t ATA_DEVICE_FAULT = 0x04'0000'01;
+    constexpr status_t ATA_BAD_SECTOR   = 0x04'0000'02;
+} // namespace status
