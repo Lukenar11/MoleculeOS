9790b37335bac18250092274589ce8c62127bc89
Luke Matthes <LukeMatthes@web.de>
2026-08-30 14:14:40 +0200

    Implement DocBlocks in "text_output".

--- runtime/stdlib/include/text_output.hpp
+++ runtime/stdlib/include/text_output.hpp
@@ -22,12 +22,14 @@ NOTES:
     because they are so small that the compiler can inline them.
 */
 
+
 #pragma once
 
 #include <types.hpp>
 #include <drivers.hpp>
 #include <sal.hpp>
 #include <array.hpp>
+
 
 namespace stdlib 
 {
@@ -42,64 +44,179 @@ namespace stdlib
                                                 drivers::vga::Text_Mode_Colors::BLACK,
                                                 does_blink);
 
-        static
-        uint32_t convert_base_to_digits(_OUT_ stdlib::Array<char, 32>& buffer,
-                                        _IN_ uint32_t value,
-                                        _IN_  const uint32_t base) noexcept;
 
-        static 
-        void put_base(_IN_ uint32_t value, 
-                      _IN_ const uint32_t base) noexcept;
+        /** 
+         * @brief Casts a integer with 
+         *        a specific base to a string (eg. base 10 -> Decimal).
+         * 
+         * @note The resulting digits are stored in reverse order.
+         * 
+         * @warning No bounds checking is performed. 
+         *          If the buffer is too small, the output will be truncated.
+         * 
+         * @param buffer casted integer
+         * @param value  integer for casting
+         * @param base   integer base
+         * 
+         * @return size of `buffer`
+         */
+        static uint32_t 
+        convert_base_to_digits(_OUT_ stdlib::Array<char, 32>& buffer,
+                               _IN_  uint32_t value,
+                               _IN_  const uint32_t base) noexcept;
+
+
+        /** 
+         * @brief Outputs a integer to the terminal with 
+         *        a specific number base (eg. base 10 -> Decimal). 
+         * 
+         * @warning Bases outside the range [2...16] are ignored.
+         * 
+         * @param value integer output
+         * @param base  integer base
+         */
+        static void 
+        put_base(_IN_ uint32_t value, 
+                 _IN_ const uint32_t base) noexcept;
             
-        static
-        uint32_t convert_uint_to_digits(_OUT_ stdlib::Array<char, 12>& buffer,
-                                        _IN_  uint32_t value) noexcept;
 
-        static void handle_backspace() noexcept;
-        static void handle_tab() noexcept;
-        static void handle_normal_char(_IN_ const char symbol) noexcept;
+        /** 
+         * @brief Casts a unsigned integer to a string.
+         * 
+         * @note The resulting digits are stored in reverse order.
+         * 
+         * @warning No bounds checking is performed. 
+         *          If the buffer is too small, the output will be truncated.
+         * 
+         * @param buffer casted integer
+         * @param value  integer for casting
+         * 
+         * @return size of `buffer`
+         */
+        static uint32_t 
+        convert_uint_to_digits(_OUT_ stdlib::Array<char, 12>& buffer,
+                               _IN_  uint32_t value) noexcept;
 
-        static 
-        uint32_t calculate_needed_lines(_IN_ const char* text) noexcept;
 
-        static 
-        void new_line() noexcept;
+        /**
+         * @brief Outputs `DELETE` at a specific position and 
+         *        a new line if the screen width is reached.
+         * 
+         * @param symbol character output
+         */
+        static void 
+        handle_backspace() noexcept;
+        
+        
+        /**
+         * @brief Outputs `TAB` at a specific position and 
+         *        a new line if the screen width is reached.
+         * 
+         * @param symbol character output
+         */
+        static void 
+        handle_tab() noexcept;
+        
 
-    public:            
-        [[nodiscard]] static inline 
-        uint32_t get_cursor_x() noexcept {
+        /**
+         * @brief Outputs normal characters at a specific position and 
+         *        a new line if the screen width is reached.
+         * 
+         * @param symbol character output
+         */
+        static void 
+        handle_normal_char(_IN_ const char symbol) noexcept;
+
+
+        /** 
+         * @brief Calculates all needed lines for a text output.
+         * 
+         * @note This function does not modify the cursor position.
+         * 
+         * @param text text for the line calculation
+         * 
+         * @return the count of needed lines.
+         */
+        static uint32_t 
+        calculate_needed_lines(_IN_ const char* text) noexcept;
+
+
+        /** 
+         * Makes a cursor Jump to the next line.
+         */
+        static void 
+        new_line() noexcept;
+
+
+    public:
+        /** 
+         * @brief Gets the X coordinate of the cursor
+         * 
+         * @return cursor X coordinate
+         */        
+        _API_ [[nodiscard]] static inline uint32_t 
+        get_cursor_x() noexcept {
             return cursor_x;
         }
 
-        [[nodiscard]] static inline 
-        uint32_t get_cursor_y() noexcept {
+
+        /** 
+         * @brief Gets the Y coordinate of the cursor
+         * 
+         * @return cursor Y coordinate
+         */   
+        _API_ [[nodiscard]] static inline uint32_t 
+        get_cursor_y() noexcept {
             return cursor_y;
         }
 
-        [[nodiscard]] static inline 
-        uint8_t get_text_color() noexcept {
+
+        /**
+         * @brief Gets the current text color.
+         * 
+         * @return text color
+         */
+        _API_ [[nodiscard]] static inline uint8_t 
+        get_text_color() noexcept {
             return cursor_color;
         }
 
-        static inline 
-        void set_text_color(_IN_ const drivers::vga::Text_Mode_Colors& color,
-                            _IN_ const drivers::vga::Text_Mode_Colors& background,
-                            _IN_ const bool does_blink=false) 
-                            noexcept {
+
+        /** 
+         * @brief Sets a new color for any other Terminal outputs.
+         * 
+         * @param color      color for symbols
+         * @param background background color for symbols
+         */
+        _API_ static inline void 
+        set_text_color(_IN_ const drivers::vga::Text_Mode_Colors& color,
+                       _IN_ const drivers::vga::Text_Mode_Colors& background,
+                       _IN_ const bool does_blink=false) noexcept {
             cursor_color = drivers::vga::Text_Mode::make_color(color, 
                                                                background, 
                                                                does_blink);
         }
 
-        static inline constexpr 
-        void set_cursor(_IN_ const uint32_t x, 
-                        _IN_ const uint32_t y) noexcept {
+
+        /** 
+         * @brief Sets a new cursor position.
+         * 
+         * @param x X coordinate
+         * @param y Y coordinate
+         */
+        _API_ static inline constexpr void
+        set_cursor(_IN_ const uint32_t x, 
+                   _IN_ const uint32_t y) noexcept {
             cursor_x = x;
             cursor_y = y;
         }
 
-        static inline
-        void reset() noexcept {
+
+        /**
+         * @brief Clears the screen and resets the cursor.
+         */
+        _API_ static inline void 
+        reset() noexcept {
             using namespace drivers;
 
             cursor_x = 0;
@@ -108,33 +225,94 @@ namespace stdlib
             vga::Text_Mode::clear_screen(vga::Text_Mode_Colors::BLACK);
         }
 
-        static void put_int(_IN_  int32_t value) noexcept;
-        static void put_uint(_IN_ uint32_t value) noexcept;
 
-        static inline
-        void put_hex(_IN_ const uint32_t value) noexcept {
+        /** 
+         * @brief Puts a signed integer.
+         * 
+         * @param value integer output
+         */
+        _API_ static void 
+        put_int(_IN_  int32_t value) noexcept;
+
+
+        /** 
+         * @brief Outputs a unsigned integer to the terminal.
+         * 
+         * @param value integer output
+         */
+        _API_ static void 
+        put_uint(_IN_ const uint32_t value) noexcept;
+
+
+        /** 
+         * @brief Outputs a hexadecimal number to the terminal.
+         * 
+         * @param value number output
+         */
+        _API_ static inline void
+        put_hex(_IN_ const uint32_t value) noexcept {
             put_string("0x");
             put_base(value, 16);
         }
 
-        static inline
-        void put_bin(_IN_ const uint32_t value) noexcept {
+
+        /** 
+         * @brief Outputs a binary number to the terminal.
+         * 
+         * @param value number output
+         */
+        _API_ static inline void 
+        put_bin(_IN_ const uint32_t value) noexcept {
             put_string("0b");
             put_base(value, 2);
         }
 
-        static inline
-        void put_ptr(_IN_ const uint32_t value) noexcept {
+
+        /** 
+         * @brief Outputs the address of pointer to the 
+         *        terminal in hexadecimal.
+         * 
+         * @param value address output
+         */
+        _API_ static inline void 
+        put_ptr(_IN_ const uint32_t value) noexcept {
             put_string("0x");
             put_base(value, 16);
         }
 
-        static 
-        void put_char(_IN_ const char symbol) noexcept;
 
-        static 
-        void put_string(_IN_ const char* message) noexcept;
+        /** 
+         * @brief Outputs a character to the terminal.
+         * 
+         * @param symbol character output
+         * 
+         * @note Allows the special characters:
+         * @note - `\r` -> Cursor jump to the line begin.
+         * @note - `\b` -> Delete the last character.
+         * @note - `\t` -> Outputs a TAB.
+         * @note - `\n` -> Cursor jump to the next line.
+         * @note - `\\"` -> Allows `"` as character output.
+         */
+        _API_ static void 
+        put_char(_IN_ const char symbol) noexcept;
+
+
+        /** 
+         * @brief Outputs a string to the terminal.
+         * 
+         * @param message string output
+         * 
+         * @note Allows the special characters:
+         * @note - `\r` -> Cursor jump to the line begin.
+         * @note - `\b` -> Delete the last character.
+         * @note - `\t` -> Outputs a TAB.
+         * @note - `\n` -> Cursor jump to the next line.
+         * @note - `\\"` -> allows `"` in the string output
+         */
+        _API_ static void 
+        put_string(_IN_ const char* message) noexcept;
     
+
         Text_Output() noexcept  = default;
         ~Text_Output() noexcept = default;
     };
--- runtime/stdlib/src/text_output.cpp
+++ runtime/stdlib/src/text_output.cpp
@@ -22,13 +22,31 @@ NOTES:
     because they are so small that the compiler can inline them.
 */
 
+
 #include <text_output.hpp>
+
 
 namespace stdlib
 {
-    uint32_t Text_Output::convert_base_to_digits(_OUT_ stdlib::Array<char, 32>& buffer,
-                                                 _IN_ uint32_t value,
-                                                 _IN_  const uint32_t base) noexcept {
+    /** 
+     * @brief Casts a integer with 
+     *        a specific base to a string (eg. base 10 -> Decimal).
+     * 
+     * @note The resulting digits are stored in reverse order.
+     * 
+     * @warning No bounds checking is performed. 
+     *          If the buffer is too small, the output will be truncated.
+     * 
+     * @param buffer casted integer
+     * @param value  integer for casting
+     * @param base   integer base
+     * 
+     * @return size of `buffer`
+     */
+    uint32_t 
+    Text_Output::convert_base_to_digits(_OUT_ stdlib::Array<char, 32>& buffer,
+                                        _IN_  uint32_t value,
+                                        _IN_  const uint32_t base) noexcept {
         const char null_char = '0';
         const char a_char    = 'A';
         const uint32_t ten   = 10;
@@ -55,8 +73,19 @@ namespace stdlib
         return i;
     }
 
-    void Text_Output::put_base(_IN_ uint32_t value, 
-                               _IN_ const uint32_t base) noexcept {
+
+    /** 
+     * @brief Outputs a integer to the terminal with 
+     *        a specific number base (eg. base 10 -> Decimal). 
+     * 
+     * @warning Bases outside the range [2...16] are ignored.
+     * 
+     * @param value integer output
+     * @param base  integer base
+     */
+    void 
+    Text_Output::put_base(_IN_ uint32_t value, 
+                          _IN_ const uint32_t base) noexcept {
         stdlib::Array<char, 32> buffer;
         uint32_t count;
 
@@ -79,8 +108,23 @@ namespace stdlib
         return;
     }
 
-    uint32_t Text_Output::convert_uint_to_digits(_OUT_ stdlib::Array<char, 12>& buffer,
-                                                 _IN_  uint32_t value) noexcept {
+
+    /** 
+     * @brief Casts a unsigned integer to a string.
+     * 
+     * @note The resulting digits are stored in reverse order.
+     * 
+     * @warning No bounds checking is performed. 
+     *          If the buffer is too small, the output will be truncated.
+     * 
+     * @param buffer casted integer
+     * @param value  integer for casting
+     * 
+     * @return size of `buffer`
+     */
+    uint32_t 
+    Text_Output::convert_uint_to_digits(_OUT_ stdlib::Array<char, 12>& buffer,
+                                        _IN_  uint32_t value) noexcept {
         const char null_char = '0';
         const uint32_t ten   = 10;
 
@@ -98,7 +142,15 @@ namespace stdlib
         return i;
     }
 
-    void Text_Output::handle_backspace() noexcept {
+
+    /**
+     * @brief Outputs `DELETE` at a specific position and 
+     *        a new line if the screen width is reached.
+     * 
+     * @param symbol character output
+     */
+    void 
+    Text_Output::handle_backspace() noexcept {
         if (cursor_x > 0) [[likely]] {
             cursor_x--;
             drivers::vga::Text_Mode::put_char_at(' ',
@@ -108,7 +160,15 @@ namespace stdlib
         }
     }
 
-    void Text_Output::handle_tab() noexcept {
+
+    /**
+     * @brief Outputs `TAB` at a specific position and 
+     *        a new line if the screen width is reached.
+     * 
+     * @param symbol character output
+     */
+    void 
+    Text_Output::handle_tab() noexcept {
         for (uint32_t i = 0; i < 4; ++i) [[likely]] {
             drivers::vga::Text_Mode::put_char_at(' ',
                                         cursor_color,
@@ -122,7 +182,15 @@ namespace stdlib
         }
     }
 
-    void Text_Output::handle_normal_char(_IN_ const char symbol) noexcept {
+
+    /**
+     * @brief Outputs normal characters at a specific position and 
+     *        a new line if the screen width is reached.
+     * 
+     * @param symbol character output
+     */
+    void 
+    Text_Output::handle_normal_char(_IN_ const char symbol) noexcept {
         drivers::vga::Text_Mode::put_char_at(symbol,
                                              cursor_color,
                                              cursor_x,
@@ -134,8 +202,18 @@ namespace stdlib
         }
     }
 
-    uint32_t Text_Output::calculate_needed_lines(_IN_ const char* text) 
-                                                 noexcept {
+
+    /** 
+     * @brief Calculates all needed lines for a text output.
+     * 
+     * @note This function does not modify the cursor position.
+     * 
+     * @param text text for the line calculation
+     * 
+     * @return the count of needed lines.
+     */
+    uint32_t 
+    Text_Output::calculate_needed_lines(_IN_ const char* text) noexcept {
         const uint32_t null   = 0;
         uint32_t needed_lines = 1;
         uint32_t x            = cursor_x;
@@ -159,7 +237,12 @@ namespace stdlib
         return needed_lines;
     }
 
-    void Text_Output::new_line() noexcept {
+
+    /** 
+     * Makes a cursor Jump to the next line.
+     */
+    void 
+    Text_Output::new_line() noexcept {
         cursor_x = 0;
         cursor_y++;
 
@@ -168,7 +251,14 @@ namespace stdlib
         }
     }
 
-    void Text_Output::put_int(_IN_ int32_t value) noexcept {
+
+    /** 
+     * @brief Puts a signed integer.
+     * 
+     * @param value integer output
+     */
+    _API_ void 
+    Text_Output::put_int(_IN_ int32_t value) noexcept {
         if (value < 0) {
             put_char('-');
 
@@ -180,7 +270,14 @@ namespace stdlib
         }
     }
 
-    void Text_Output::put_uint(_IN_ uint32_t value) noexcept {
+
+    /** 
+     * @brief Outputs a unsigned integer to the terminal.
+     * 
+     * @param value integer output
+     */
+    _API_ void 
+    Text_Output::put_uint(_IN_ const uint32_t value) noexcept {
         uint32_t count;
         stdlib::Array<char, 12> buffer;
         
@@ -199,7 +296,21 @@ namespace stdlib
         return;
     }
 
-    void Text_Output::put_char(_IN_ const char symbol) noexcept {
+
+    /** 
+     * @brief Outputs a character to the terminal.
+     * 
+     * @param symbol character output
+     * 
+     * @note Allows the special characters:
+     * @note - `\r` -> Cursor jump to the line begin.
+     * @note - `\b` -> Delete the last character.
+     * @note - `\t` -> Outputs a TAB.
+     * @note - `\n` -> Cursor jump to the next line.
+     * @note - `\\"` -> Allows `"` as character output.
+     */
+    _API_ void 
+    Text_Output::put_char(_IN_ const char symbol) noexcept {
         switch (symbol) {
         case '\r':
             cursor_x = 0;
@@ -223,7 +334,21 @@ namespace stdlib
         }
     }
 
-    void Text_Output::put_string(_IN_ const char* message) noexcept {
+
+    /** 
+     * @brief Outputs a string to the terminal.
+     * 
+     * @param message string output
+     * 
+     * @note Allows the special characters:
+     * @note - `\r` -> Cursor jump to the line begin.
+     * @note - `\b` -> Delete the last character.
+     * @note - `\t` -> Outputs a TAB.
+     * @note - `\n` -> Cursor jump to the next line.
+     * @note - `\\"` -> allows `"` in the string output
+     */
+    _API_ void
+    Text_Output::put_string(_IN_ const char* message) noexcept {
         const uint32_t needed_lines = calculate_needed_lines(message);
         const uint32_t remaining    = drivers::vga::TEXT_MODE_SCREEN_HEIGHT - 
                                       cursor_y;
